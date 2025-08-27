cde7a3b02fafb1f1f017124d26aae1bc701c064d
Ahmad Izzat <freshicedlemontea@gmail.com>
2025-08-27 15:00:38 +0800

    Adjusted nutritional values for family `need` and `stockpile` in `GameFactory` to align with updated scaling standards.

--- src/main/scala-3/ahmad/GameFactory.scala
+++ src/main/scala-3/ahmad/GameFactory.scala
@@ -27,10 +27,10 @@ object GameFactory {
     def x3(n: Nutrition)   = Nutrition(n.cal*3, n.protein*3, n.carbs*3, n.vitamins*3)
 
     val families = Vector(
-      new Family("Ali",  need(Nutrition(1800,50,250,60)), stockpile = x3(Nutrition(1800,50,250,60))),
-      new Family("Bala", need(Nutrition(2000,60,300,70)), stockpile = x3(Nutrition(2000,60,300,70))),
-      new Family("Chen", need(Nutrition(1700,45,230,55)), stockpile = x3(Nutrition(1700,45,230,55))),
-      new Family("Devi", need(Nutrition(1900,55,270,65)), stockpile = x3(Nutrition(1900,55,270,65)))
+      new Family("Ali",  need(Nutrition(900, 25, 125, 30)), stockpile = x3(Nutrition(900, 25, 125, 30))),
+      new Family("Bala", need(Nutrition(1000, 30, 150, 35)), stockpile = x3(Nutrition(1000, 30, 150, 35))),
+      new Family("Chen", need(Nutrition(850, 25, 120, 20)), stockpile = x3(Nutrition(850, 25, 120, 20))),
+      new Family("Devi", need(Nutrition(950, 55, 135, 30)), stockpile = x3(Nutrition(950, 55, 135, 30)))
     )
 
     val gs = new GameState(
